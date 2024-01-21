177ff78c7d051c75c5e7c5a384f042c3e3a63296
no382001 <102482527+no382001@users.noreply.github.com>
2024-01-21 19:14:03 +0100

    symlinked plogl

--- plogl/pack.pl
+++
@@ -1,8 +0,0 @@
-name(plOpenGL).
-version('0.6.2').
-title('OpenGL Bindings').
-keywords([opengl,graphics,games,bindings]).
-author( 'Jan Tatham', 'jan@sebity.com' ).
-home('http://www.sebity.com' ).
-download( 'http://www.sebity.com/downloads/plOpenGL-0.6.2.zip' ).
-
